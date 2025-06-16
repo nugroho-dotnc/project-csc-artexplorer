3be981571ae24c673cae793e6afbddda30933bb6
naufalbintang <naufalbintangpradana@gmail.com>
2025-06-16 23:51:14 +0700

    - Memodifikasi page /src/pages/about/index.js

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -3,7 +3,18 @@ import React from "react";
 const About = () => {
     return (
         <>
-            <h1>Hello World</h1>
+            {/* container tentang kami */}
+            <div className="h-dvh w-full bg-white px-[96px]">
+                <div className="pt-32 text-center">
+                    <p className="text-secondary text-[40px] font-bold">Tentang Kami</p>
+                    <p className="text-secondary text-[20px]">Platform digital ini memperluas akses terhadap koleksi seni dan budaya museum. Jelajahi galeri virtual kami yang kaya akan detail, kenali visi dan misi institusi di halaman "Tentang Kami", dan hubungi kami melalui formulir kontak. Situs ini dirancang agar responsif dan mudah diakses di semua perangkat.</p>
+                </div> 
+                <div className="bg-red-300 h-[849px] w-[461px]"></div>
+            </div>
+            {/* container anggota kami */}
+            <div>
+
+            </div>
         </>
     );
 };
