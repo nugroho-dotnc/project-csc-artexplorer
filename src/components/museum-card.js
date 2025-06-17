6ecb9c351c175e947d0339584ab1c6d1de2baaaf
nugroho <nugrohonug066@gmail.com>
2025-06-17 18:50:12 +0700

    fixing museum pages

--- src/components/museum-card.js
+++ src/components/museum-card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const MuseumCard = ({ image, title, desc, link }) => {
+const MuseumCard = ({ image, title, desc, link, expanded = false }) => {
   return (
-    <a href={link}>
+    <a href={link} className='p-4'>
       <div
-        className="h-[28rem] w-80 rounded-xl relative transition-transform duration-300 ease-in-out hover:-translate-y-2 bg-cover bg-center"
+        className={`h-[28rem] ${!expanded?"w-80":"w-full"} rounded-xl overflow-hidden relative transition-transform duration-300 ease-in-out hover:-translate-y-2 bg-cover bg-center`}
         style={{ backgroundImage: `url(${image})` }}
       >
         <div className="absolute bottom-0 h-28 w-full bg-black/80 p-2 overflow-hidden text-shadow-xs text-shadow-primary-100">
